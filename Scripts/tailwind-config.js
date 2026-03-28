a8960be6afb72723a7731ec354c03e2c7d11d584
Nathan Flack <nathanjflack@gmail.com>
2026-03-28 14:04:49 +0800

    converted from base css to tailwind, creating a configuration file for our custom fonts and colors

---
+++ Scripts/tailwind-config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'background-black': '#07070c',
+        'neon-green': '#3Dff6e',
+      },
+      fontFamily: {
+        'syne': ['Syne-ExtraBold', 'sans-serif'],
+      }
+    }
+  }
+}
