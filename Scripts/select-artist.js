9f5834b275a763c26822e5f611a3f54162ed911c
Nathan Flack <nathanjflack@gmail.com>
2026-04-10 09:58:01 +0800

    made function for getting album art from apple

--- Scripts/select-artist.js
+++ Scripts/select-artist.js
@@ -7,6 +7,30 @@ const startButton = document.getElementById("startButton");
 const chosenArtists = [];
 
 
+async function GetArtistImageFromApple(artistId) {
+    try {
+        // just change entity=song to album
+        const url = `https://itunes.apple.com/lookup?id=${artistId}&entity=album&limit=1`;
+        const res = await fetch(url);
+        const data = await res.json();
+        
+        if (data.results && data.results.length > 1) {
+            // API returns results as a list with album details as the second item
+            const albumData = data.results[1]; 
+            
+            if (albumData.artworkUrl100) {
+                const highResImage = albumData.artworkUrl100.replace('100x100bb', '600x600bb');
+                return highResImage;
+            }
+        }
+        
+        return "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png";
+    } catch (err) {
+        console.error("Failed to fetch image from Apple:", err);
+        return "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png";
+    }
+}
+
 async function searchArtistsFromAPI(query) {
     if (!query) {
         // clear ui if nothing is queried / query is empty
