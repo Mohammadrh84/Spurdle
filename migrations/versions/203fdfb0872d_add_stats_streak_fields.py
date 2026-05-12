2df8a9aa2cd59705456881b6314f9155c0bec20c
surtaj singh <surtazsingh40@gmail.com>
2026-05-12 18:09:11 +0800

    Set default values for streak migration columns

--- migrations/versions/203fdfb0872d_add_stats_streak_fields.py
+++ migrations/versions/203fdfb0872d_add_stats_streak_fields.py
@@ -19,10 +19,20 @@ depends_on = None
 def upgrade():
     with op.batch_alter_table("stats", schema=None) as batch_op:
         batch_op.add_column(
-            sa.Column("current_streak", sa.Integer(), nullable=True)
+            sa.Column(
+                "current_streak",
+                sa.Integer(),
+                nullable=False,
+                server_default="0"
+            )
         )
         batch_op.add_column(
-            sa.Column("best_streak", sa.Integer(), nullable=True)
+            sa.Column(
+                "best_streak",
+                sa.Integer(),
+                nullable=False,
+                server_default="0"
+            )
         )
 
 
